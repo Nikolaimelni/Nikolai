27f275b8c0bf651786ec22f7c608bb9139106fb8
Nikolai Melnichenko <nikolaymelni@gmail.com>
2024-01-19 15:55:43 +0100

    almost done

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,58 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleButtons = document.querySelectorAll('.toggle-info');
+    var responsiveLists = document.querySelectorAll('.responsive-list');
+
+    toggleButtons.forEach(function(toggleButton) {
+        toggleButton.addEventListener('click', function() {
+            var responsiveList = this.parentNode;
+            var isListOpen = responsiveList.classList.contains('open');
+            responsiveList.classList.toggle('open');
+            
+            this.textContent = isListOpen ? 'Tap to open description' : 'Tap to hide description';
+        });
+    });
+
+    function checkSize() {
+        responsiveLists.forEach(function(responsiveList) {
+            var toggleButton = responsiveList.querySelector('.toggle-info');
+            if (window.innerWidth > 768) {
+                responsiveList.classList.add('open');
+            } else {
+                responsiveList.classList.remove('open');
+                toggleButton.textContent = 'Tap to open description';
+            }
+        });
+    }
+
+    window.addEventListener('resize', checkSize);
+    checkSize();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Получаем элементы бургер-меню и ссылок
+    var navbarCollapse = document.querySelector(".navbar-collapse");
+    var navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function() {
+            navbarCollapse.classList.remove("show");
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var elementsToAnimate = document.querySelectorAll('.hidden');
+
+    function checkVisibility() {
+        elementsToAnimate.forEach(function(element) {
+            var rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight) {
+                element.classList.add('visible');
+            }
+        });
+    }
+
+    checkVisibility();
+
+    window.addEventListener('scroll', checkVisibility);
+});
